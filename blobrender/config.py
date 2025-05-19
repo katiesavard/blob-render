6565419dce3507cbfc834fc8aec07cc9b1752b67
Katie Savard <k8tsavard@gmail.com>
2025-05-19 16:26:51 +0100

    file for specifying directories needed for reading data and saving plots

---
+++ blobrender/config.py
@@ -0,0 +1,11 @@
+import os
+
+CWD = os.getcwd()
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+HOME = os.path.expanduser('~')
+
+TOOLS = os.path.join(BASE_DIR, 'tools')
+CONTAINERS = os.path.join(BASE_DIR, 'containers')
+SIM_DAT = os.path.join(BASE_DIR, 'sim_data')
+TEL_INFO = os.path.join(BASE_DIR, 'tel_info')
+PLOTS = os.path.join(BASE_DIR, 'plots')
