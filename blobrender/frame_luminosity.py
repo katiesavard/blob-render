310d2f9d505a3a85699d7f04ba12aba91619dc82
Katie Savard <k8tsavard@gmail.com>
2025-05-19 16:46:20 +0100

    made find_limits_com() to not include magic numbers, defined theta to be angle to the LOS, moved loader_bar and interpolate_cyl_to_cart to tools, changed input mechanism to parser.add_argument, changed to old definition of theta (long list of thetas to interpolate on) to be defines as thetas instead

--- blobrender/frame_luminosity.py
+++ blobrender/frame_luminosity.py
@@ -1,16 +1,9 @@
-import sys
-import pyPLUTO as pypl
-import pyPLUTO.pload as pp
 import tools
-#from pyplutplot import *
-wdir = '/pluto_playtime/data_storage/' #set up working directory where data is stored
-import matplotlib.pyplot as plt
-import scipy
-from scipy.interpolate import griddata
-#from pluto_luminosity_conversion import *
-
-
-
+import numpy as np 
+import argparse
+import yaml
+import os
+from .config import SIM_DAT, PLOTS
 
 
 def find_limits(v,x1,x2,buffer_size,minimum_size):
@@ -73,17 +66,13 @@ def find_limits(v,x1,x2,buffer_size,minimum_size):
 
     return stop_index_top, stop_index_bottom, stop_index_side
     
-def find_limits_com(image_timestep,d_vals):
-    stop_index_side = 650
-    minimum_size = 1300
-    stop_index_bottom = (int(d_vals[image_timestep])*6-int(minimum_size/2))
-    stop_index_top = int(d_vals[image_timestep])*6+int(minimum_size/2)
+def find_limits_com(stop_index_side,minimum_size,centre,res):
+    stop_index_bottom = (int(centre)*res-int(minimum_size/2))
+    stop_index_top = int(centre)*res+int(minimum_size/2)
     stop_index_bottom
     return stop_index_top, stop_index_bottom, stop_index_side
 
-
-
-def ordered_cylindrical_grid(theta_len,z_len,r_len,theta,z,r,values):
+def ordered_cylindrical_grid(theta_len,z_len,r_len,thetas,z,r,values):
     # creating long 2D arrays which correspond to each point in the frame -> ordered by constant z 
     rr = []
     zz = []
@@ -103,7 +92,7 @@ def ordered_cylindrical_grid(theta_len,z_len,r_len,theta,z,r,values):
             arr_l = np.repeat(values[j][i],theta_len).tolist()
             r_ordered.append(arr_r) # r at i (const in j loop)
             z_ordered.append(arr_z) # z at j (iterating through const r)
-            theta_ordered.append(theta) # for all theta
+            theta_ordered.append(thetas) # for all theta
             l_ordered.append(arr_l)
         rr.append(np.asarray(r_ordered).flatten())
         zz.append(np.asarray(z_ordered).flatten())
@@ -112,10 +101,10 @@ def ordered_cylindrical_grid(theta_len,z_len,r_len,theta,z,r,values):
     
     return rr,zz,tt,ll
 
-def ordered_cartesian_grid(ax1,ax2,theta,z_cart):
+def ordered_cartesian_grid(ax1,ax2,thetas,z_cart):
     z_ordered = []
     num_z = len(ax2)
-    num_xtheta = len(ax1)*len(theta)
+    num_xtheta = len(ax1)*len(thetas)
 
     for i in range(num_z):
         z_ = z_cart[(0+(num_xtheta*i)):(0+(num_xtheta*(i+1)))]
@@ -124,45 +113,6 @@ def ordered_cartesian_grid(ax1,ax2,theta,z_cart):
     return z_ordered
 
 
-def loader_bar(i,range,modulo): #just for output purposes
-    perc = int((i/range)*100)
-    prev_perc = int(((i-1)/range)*100)
-    if i==0:
-        s = str(0)+"%"
-        print(s,end="...",flush=True)
-    if perc%modulo==0 and perc!=prev_perc:
-        s = str(perc)+"%"
-        print(s,end="...",flush=True)
-        prev_perc=perc+modulo
-
-def interpolate_cyl_to_cart(x_cart,y_cart,z_ordered,ll,grid_x,grid_y,grid_size,system_name,results_folder,image_timestep,save='save'):
-    if save=='save':
-        interps = []
-        iter_num = len(z_ordered)
-        for i in range(iter_num):
-            loader_bar(i,iter_num,5)
-            x_arr = x_cart[i].flatten() #160x130 points -> 20800
-            y_arr = y_cart[i].flatten()
-            points = (x_arr,y_arr)
-            values = ll[i].flatten()
-            interp_grid = (grid_x,grid_y)
-            interp = griddata(points,values,interp_grid,method='linear')
-            interps.append(interp)
-        interp_clean = np.nan_to_num(interps) #remove all nans and turn them into zeros for the sake of integration 
-        save_list(np.asarray(interp_clean),results_folder,'interpolated_frame_'+system_name+'_'+str(image_timestep))
-        len(interp_clean)
-        integrated_frames = []
-        pixel_vol = (grid_size)**3 #multiply by the volume of each pixel when integrating
-        for frame in interp_clean:
-            integrated_frame = np.sum(frame,axis=0)*pixel_vol*2
-            integrated_frames.append(integrated_frame)
-        save_list(np.asarray(integrated_frames),results_folder,'integrated_frame_'+system_name+'_'+str(image_timestep))
-    elif save=='load':
-        print("loading")
-        interp_clean = load_list(results_folder,'interpolated_frame_'+system_name+'_'+str(image_timestep))
-        integrated_frames = load_list(results_folder,'integrated_frame_'+system_name+'_'+str(image_timestep))
-    return interp_clean, integrated_frames
-
 
 
 def main():
@@ -170,58 +120,63 @@ def main():
     ###########   input arguments    ###############
 
     """
-    You can input arguments on the command line OR just default to the ones listed here
+    You can input arguments on the command line OR just use default to the ones in default.framelum.yaml
+    The default values are set in the YAML file, which is loaded at the beginning of the script.
     """
+    # Load defaults from YAML
+    with open('default_framelum.yaml', 'r') as f:
+        defaults = yaml.safe_load(f)
 
-    args = sys.argv[1:]
-
-    default_system_name = 'ri0.001_rb0.02_lz2' #where the PLUTO output data is stored (name of the folder)
-    default_image_timestep = 150 #which timestep
-
-
-    if len(args)==2:
-        system_name = args[0] #'ri0.001_rb0.02_k0.1_lz25_6hhres_newprs2'
-        image_timestep = args[1]
-    else:
-        default = input('need 2 args: use default arguments? '+'\n'+'default arguments are: '+'\n'+default_system_name+'\n'+'timestep '+str(default_image_timestep)+'\n'+'(y/n)')
-        if default=='y':
-            system_name = default_system_name
-            image_timestep = default_image_timestep
-        else:
-            return
-
-    save_or_load = input('save or load interpolation?')
-    if save_or_load!= 'save' and save_or_load!='load':
-        print('bad input: save or load!')
-        return
-    
     ###########   definitions    ###############
+    parser = argparse.ArgumentParser(description="Frame luminosity analysis")
+    parser.add_argument('--system_name', type=str, default=defaults['system_name'], help='PLUTO output folder name')
+    parser.add_argument('--image_timestep', type=int, default=defaults['image_timestep'], help='Timestep to analyse')
+    parser.add_argument('--kappa', type=float, default=defaults['kappa'], help='Fraction of pressure contribution to luminosity')
+    parser.add_argument('--theta', type=float, default=defaults['theta'], help='Angle to the line of sight (degrees)')
+    parser.add_argument('--gamma', type=float, default=defaults['gamma'], help='Lorentz factor')
+    parser.add_argument('--distance_in_pc', type=float, default=defaults['distance_in_pc'], help='Distance in parsecs')
+    parser.add_argument('--alpha', type=float, default=defaults['alpha'], help='Spectral index')
+    parser.add_argument('--P_sim', type=float, default=defaults['P_sim'], help='PLUTO units: pressure (kg m^-1 s^2)')
+    parser.add_argument('--L_sim', type=float, default=defaults['L_sim'], help='PLUTO units: length (m)')
+    parser.add_argument('--nu_observe', type=float, default=defaults['nu_observe'], help='Observing frequency (Hz)')
+    parser.add_argument('--eta', type=float, default=defaults['eta'], help='Equipartition factor')
+    parser.add_argument('--dtype', type=str, default=defaults['dtype'], help='PLUTO output data type (flt,dbl,hdf5)')
+    parser.add_argument('--load_interp', action='store_true', default=defaults['load_interp'], help='Set this flag to load interpolation (default: False)')
 
-    kappa = 0.1 #fraction of pressure contribution to luminosity
-    gamma = 2.0 #lorentz factor
-    distance_in_pc = 2960
-    alpha= -0.507 #spectral index
-    P_sim = 1.503*10**(-4) #kg m^-1 s^2  (PLUTO units)
-    L_sim = 1*10**13 #m (PLUTO units)
-    nu_observe = (1.28* 1e9) #1.28 GHz (Bright2020) observing frequency
-    eta=0.75 #equipartition
+    args = parser.parse_args()
+    # Unpack arguments
+    system_name = args.system_name
+    image_timestep = args.image_timestep
+    kappa = args.kappa
+    theta = args.theta
+    gamma = args.gamma
+    distance_in_pc = args.distance_in_pc
+    alpha = args.alpha
+    P_sim = args.P_sim
+    L_sim = args.L_sim
+    nu_observe = args.nu_observe
+    eta = args.eta
+    dtype = args.dtype
+    load_interp = args.load_interp
+
+    #some calculated and related values
     exponent = (3-alpha)/2
     p = (4*exponent) - 5 #2.01
-    dtype = 'flt' #what data type PLUTO outputs
+    viewing_angle = (2*np.pi)/360 *theta
+    
 
     ###########    setups    ###############
-    data_dir = '/pluto_playtime/data_storage/'+system_name+'/' #directory where data is stored
-    results_folder = '/Users/savard/PLUTO/pluto_playtime/plotting_analysis/sim_results/{}'.format(system_name) #where you want the results to go
-    angle_degrees = tools.theta_from_beta(tools.gamma_to_beta(gamma))
-    viewing_angle = (2*np.pi)/360 *angle_degrees
+    data_dir = os.path.join(SIM_DAT, system_name)
+    results_folder = os.path.join(PLOTS, system_name) 
+
+    ###########################################################################
+     
     file_disp = 'disp_array'+str(system_name)
     init_offset = 200
-        
-    ###########################################################################
+    d_vals = tools.load_list(results_folder,file_disp)+init_offset
+    centre = d_vals[image_timestep]
 
-    d_vals = load_list(results_folder,file_disp)+init_offset
-
-    D = load_data_obj(data_dir,image_timestep,data_type=dtype)
+    D = tools.load_data_obj(data_dir,image_timestep,data_type=dtype)
     
 
     v = D.tr1*((kappa*D.prs)**exponent)
@@ -237,7 +192,11 @@ def main():
     #buffer_size = 100
     #minimum_size = 1300
     #stop_index_top, stop_index_bottom, stop_index_side = find_limits(v,x1,x2,buffer_size,minimum_size)
-    stop_index_top, stop_index_bottom, stop_index_side = find_limits_com(image_timestep,d_vals)
+
+    stop_index_side = 650
+    minimum_size = 1300
+    res = 6
+    stop_index_top, stop_index_bottom, stop_index_side = find_limits_com(stop_index_side,minimum_size,centre,res)
 
     em = (v[:stop_index_side].T[stop_index_bottom:stop_index_top]).T #psuedo emissivity, 2D array 
     ax1 = x1[:stop_index_side]
@@ -245,31 +204,31 @@ def main():
 
     crop_name = 'simimage_originalcrop_'+system_name+'_'+str(image_timestep)
     crop_cbartitle='pseudo-emissivity sim units'
-    plot_basic(ax1,ax2,em,crop_name,crop_cbartitle,system_name,results_folder) #save the cropped image original
+    tools.plot_basic(ax1,ax2,em,crop_name,crop_cbartitle,system_name,results_folder) #save the cropped image original
 
     
    ###### ######      boost emissivity        ###############################
 
     beta = (D.vx2[:stop_index_side].T[stop_index_bottom:stop_index_top]).T
-    values = doppler_boost_lum(beta,viewing_angle,alpha,em)
+    values = tools.doppler_boost_lum(beta,viewing_angle,alpha,em)
 
 
     ############      make a cylindrical grid        #########################
 
     r = ax1 #r, 160 vals
     z = ax2 #z, 1600 vals
-    theta = np.linspace(0,np.pi/2,130) #create some theta to wrap it around pi/2, arbitrary number of steps for now
-    theta_len = len(theta)
+    thetas = np.linspace(0,np.pi/2,130) #create some theta to wrap it around pi/2, arbitrary number of steps for now
+    theta_len = len(thetas)
     z_len = len(z)
     r_len = len(r)
 
-    rr, zz, tt, ll = ordered_cylindrical_grid(theta_len,z_len,r_len,theta,z,r,values)
+    rr, zz, tt, ll = ordered_cylindrical_grid(theta_len,z_len,r_len,thetas,z,r,values)
 
     #convert to cartesian:
-    x_cart, y_cart, z_cart = cyl_to_cart(rr, zz, tt)
+    x_cart, y_cart, z_cart = tools.cyl_to_cart(rr, zz, tt)
 
     #order cartesian grid:
-    z_ordered = ordered_cartesian_grid(ax1,ax2,theta,z_cart)
+    z_ordered = ordered_cartesian_grid(ax1,ax2,thetas,z_cart)
 
     #create new grid 2D to interpolate on
     x_max = y_max = np.max(r)
@@ -283,7 +242,7 @@ def main():
     ############      interpolate        ###########################################
 
     print('beginning interpolation')
-    interp_clean, integrated_frames = interpolate_cyl_to_cart(x_cart,y_cart,z_ordered,ll,grid_x,grid_y,grid_size,system_name,results_folder,image_timestep,save=save_or_load)
+    interp_clean, integrated_frames = tools.interpolate_cyl_to_cart(x_cart,y_cart,z_ordered,ll,grid_x,grid_y,grid_size,system_name,results_folder,image_timestep,load=load_interp)
 
     #plot a horizontal slice to check interpolation interpolation#
     populated_slices = []
@@ -295,32 +254,32 @@ def main():
     test_slice_index = populated_slices[int(len(populated_slices)/2)] #get a test slice about halfway through detection
     slice_name = 'simimage_interpslice_'+str(test_slice_index)+'_'+system_name+'_'+str(image_timestep)
     slice_cbartitle='pseudo emissivity sim units'
-    plot_basic(grid_x,grid_y,interp_clean[test_slice_index],slice_name,slice_cbartitle,system_name,results_folder)
+    tools.plot_basic(grid_x,grid_y,interp_clean[test_slice_index],slice_name,slice_cbartitle,system_name,results_folder)
 
     #plot a rotated slice 
     rotate_name = 'simimage_interpintegrated_'+system_name+'_'+str(image_timestep)
     rotate_cbartitle = 'pseudo emissivity sim units'
-    plot_basic(ax1,ax2,np.asarray(integrated_frames).T,rotate_name,rotate_cbartitle,system_name,results_folder)
+    tools.plot_basic(ax1,ax2,np.asarray(integrated_frames).T,rotate_name,rotate_cbartitle,system_name,results_folder)
 
     ############      convert to janskys        #######################################
-    lum_real_unit = lum_unit_si(eta,P=P_sim,L=L_sim,nu=nu_observe, q=p) #W Hz^-1 sr^-1
+    lum_real_unit = tools.lum_unit_si(eta,P=P_sim,L=L_sim,nu=nu_observe, q=p) #W Hz^-1 sr^-1
     lum_sim = np.asarray(integrated_frames)
     lum_true = lum_sim*lum_real_unit # W Hz^-1 sr^-1
     lum_jansky = lum_true*(10**26)*np.pi*4 # 10^26 W Hz^-1  -> 10^-26 W m^-2 Hz^-1 = 1 jansky 
     distance = distance_in_pc*3.086*(10**16) # convert parsec to m 
 
     boosted_lum = np.asarray(lum_jansky / (4*np.pi*(distance**2)),dtype='float64')
-    save_list(boosted_lum,results_folder,'pixel_lum_'+system_name+'_'+str(image_timestep)+'_'+str(nu_observe/(1e9))+str('GHz'))
+    tools.save_list(boosted_lum,results_folder,'pixel_lum_'+system_name+'_'+str(image_timestep)+'_'+str(nu_observe/(1e9))+str('GHz'))
 
     frame_flux = np.sum((boosted_lum*10**3))*2
     print('total luminosity in frame in mJy: '+str(frame_flux))
 
     # plot image in jaskys and arcseconds 
 
-    x = m_to_arcseconds(ax1*L_sim,distance_in_pc)
-    y = m_to_arcseconds(ax2*L_sim,distance_in_pc)
+    x = tools.m_to_arcseconds(ax1*L_sim,distance_in_pc)
+    y = tools.m_to_arcseconds(ax2*L_sim,distance_in_pc)
     radio_name = 'radio_frame_'+system_name+'_'+str(image_timestep)+'_'+str(nu_observe/(1e9))+str('GHz')
-    plot_radio(x,y,(boosted_lum*10**3).T,radio_name,'mJy per pixel',system_name,results_folder)
+    tools.plot_radio(x,y,(boosted_lum*10**3).T,radio_name,'mJy per pixel',system_name,results_folder)
 
 
 main()
