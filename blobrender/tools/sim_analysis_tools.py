a81c0f86986ffa6cc6a7598fd1173ee785df4719
Katie Savard <k8tsavard@gmail.com>
2025-05-23 15:29:51 +0100

    rgb to greyscale function added

--- blobrender/tools/sim_analysis_tools.py
+++ blobrender/tools/sim_analysis_tools.py
@@ -6,7 +6,8 @@ import pyPLUTO as pypl
 import pyPLUTO.pload as pp
 from . import basics as b
 
-
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
 
 def load_data_obj(ddir, num=-1, data_type='dbl'):
     """
