213faa7d2fc95f9c25ade3e2ea80265fdf78509e
Katie Savard <k8tsavard@gmail.com>
2025-07-17 08:56:13 +0100

    added psf output and channel flexibility

--- blobrender/predict_fromfits.py
+++ blobrender/predict_fromfits.py
@@ -2,6 +2,7 @@ import subprocess
 import os
 import stat
 from . import tools
+import numpy as np
 
 from .paths import CONFIGS, CONTAINERS, RESULTS
 from blobrender.help_strings import HELP_DICT
@@ -12,6 +13,9 @@ from blobrender.help_strings import HELP_DICT
 #populate the scale and pix from the format of the fits file
 
 #timestep is just for naming conventions 
+
+
+
 
 
 def main():
@@ -38,7 +42,8 @@ def main():
 	container_name = args.container_name
 	container_type = args.container_type
 
-	nchannels = '4'
+	nchannels = '1'
+	pixels_per_beam = 10
 
 
 	#some specific requirements that I need to figure out how they depend on the telescope 
@@ -65,8 +70,8 @@ def main():
 		raise ValueError(f"Unknown container type: {container_type}. Use 'singularity', 'docker', or 'none'")
 
 	bash_runfile = 'run_predict.sh'
-	predict_file_name = os.path.join('results','brender_'+telescopename+'_inpmodel_'+timestep)
-	image_file_name = os.path.join('results','brender_'+telescopename+'_modimage_'+timestep)
+	predict_file_name = os.path.join('results','brender_'+telescopename+'_inpmodel_'+timestep) #where you instert the simulation
+	image_file_name = os.path.join('results','brender_'+telescopename+'_modimage_'+timestep) #images from cleaning process
 	imagesum_file_name = os.path.join('results','brender_'+telescopename+'_sumimage_'+timestep)
 
 
@@ -80,7 +85,7 @@ def main():
 	
 	#clean with 0 iterations: create fits file images with dirty visibilities but the right dimensions
 	f.write('printf "creating model\n" \n')
-	f.write(container_setup+'wsclean -size '+xpix+' '+ypix+' -scale '+scale+'asec -niter 0 -channels-out '+nchannels+' '+reorder+' -name '+predict_file_name+' -data-column '+column+' -use-wgridder -mem '+mem+' '+split_ms_name+'\n')
+	f.write(container_setup+'wsclean -size '+xpix+' '+ypix+' -scale '+scale+'asec -niter 0 -make-psf -channels-out '+nchannels+' '+reorder+' -name '+predict_file_name+' -data-column '+column+' -use-wgridder -mem '+mem+' '+split_ms_name+'\n')
 	
 	#create a -model fits file with the simulated data in it with the same format as the -image file produced from the previous cleaning step 
 	f.write(
@@ -98,12 +103,16 @@ def main():
 		f.write("python3 -m blobrender.tools.change_RADEC_fits "+predict_file_name+'-dirty.fits '+newRA+' '+newDEC+'\n')
 	
 	#predict model visibilities
-	#f.write('printf "predicting model visibilities\n" \n')
-	#f.write(container_setup+'wsclean -predict -mem '+mem+' -size '+xpix+' '+ypix+' -scale '+scale+'asec -channels-out 1 '+reorder+' -name '+predict_file_name+' -use-wgridder '+split_ms_name+'/\n')
-		
+	f.write('printf "predicting model visibilities\n" \n')
+	f.write(container_setup+'wsclean -predict -mem '+mem+' -size '+xpix+' '+ypix+' -scale '+scale+'asec -channels-out '+nchannels+' '+reorder+' -name '+predict_file_name+' -use-wgridder '+split_ms_name+'/\n')
+	
+	#estimate the beam size from the PSF fits file
+	f.write('pixscale=$(python3 -m blobrender.tools.calc_beamsize --fitsfile ' + predict_file_name + '-psf.fits --pixels_per_beam ' + str(pixels_per_beam) + ')\n')
+	f.write('echo "Pixel scale is $pixscale"\n')
+
 	#image the model visibilities
-	#f.write('printf "imaging model data with no noise\n" \n')
-	#f.write(container_setup+'wsclean -mem 80 -mgain 0.9 -gain 0.15 -size 1024 1024 -scale '+imscale+' -niter 1000 -channels-out 1 -no-update-model-required '+reorder+' -name '+image_file_name+' -data-column MODEL_DATA '+field+' -use-wgridder '+split_ms_name+'\n')
+	f.write('printf "imaging model data with no noise\n" \n')
+	f.write(container_setup+'wsclean -mem 80 -mgain 0.9 -gain 0.15 -size 1024 1024 -scale ${pixscale}asec -niter 1000 -channels-out 1 -no-update-model-required '+reorder+' -name '+image_file_name+' -data-column MODEL_DATA '+field+' -use-wgridder '+split_ms_name+'\n')
 	
 	#add together model and real data
 	if add_noise:
@@ -130,7 +139,5 @@ def main():
 	#run bash file 
 	subprocess.call("./"+bash_runfile)
 
-
 if __name__ == "__main__":
     main()
-
