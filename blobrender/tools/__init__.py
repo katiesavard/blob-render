da6e68bf49d12e2db32a68ecfb0e10c72e3fe8ab
Katie Savard <k8tsavard@gmail.com>
2025-07-24 08:51:29 +0100

    image check tools now added

--- blobrender/tools/__init__.py
+++ blobrender/tools/__init__.py
@@ -2,3 +2,4 @@ from .pluto_luminosity_conversion import lum_unit_si
 from .sim_analysis_tools import (gamma_to_beta, theta_from_beta, cyl_to_cart,load_data_obj,doppler_boost_lum, interpolate_cyl_to_cart, m_to_arcseconds, angle_to_boost, beta_to_gamma,rgb2gray)
 from .plotting import plot_basic, plot_radio, save_fig
 from .basics import (save_list, load_list, loader_bar, get_arguments,update_yaml)
+from .image_checks import (baseline_range_from_ms, check_image_pixelsize, check_image_fov, get_min_max_frequency)
