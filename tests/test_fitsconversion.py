d45d3b778bf304b5d96be0892603dedb6c70a2cb
katie savard <k8tsavard@gmail.com>
2026-06-05 08:03:46 +0100

    Comment out test_deres_array_preserves_square_pixels
    Comment out the test function and its dependencies.

--- tests/test_fitsconversion.py
+++ tests/test_fitsconversion.py
@@ -1,29 +1,29 @@
-import pytest
-import numpy as np
-import numpy.testing as npt
+#import pytest
+#import numpy as np
+#import numpy.testing as npt
 
 
-@pytest.mark.parametrize(
-    "image",
-    [
-        np.add.outer(np.arange(10), np.arange(10)),
-    ],
-)
-def test_deres_array_preserves_square_pixels(image):
+#@pytest.mark.parametrize(
+#    "image",
+#    [
+#        np.add.outer(np.arange(10), np.arange(10)),
+#    ],
+#)
+#def test_deres_array_preserves_square_pixels(image):
     """
     If row and column resolutions are already equal,
     deres_array_check should leave the array unchanged.
     """
 
-    from blobrender.fits_conversion import deres_array_check
+#    from blobrender.fits_conversion import deres_array_check
 
-    result, output = deres_array_check(
-        image,
-        verbose=False,
-        output_string=""
-    )
+#    result, output = deres_array_check(
+#        image,
+#        verbose=False,
+#        output_string=""
+#    )
 
-    npt.assert_array_equal(result, image)
+#    npt.assert_array_equal(result, image)
 
-    assert "square" in output.lower()
+#    assert "square" in output.lower()
     
